490390bcd847f76788884ef67fc04862c5945620
Josh Skouson <Jmatt6836@gmail.com>
2022-04-22 11:46:13 -0600

    added css and messed with js

--- WDD230/scripts/main.js
+++ WDD230/scripts/main.js
@@ -1,9 +1,13 @@
+//variables and date object
 let date = new Date();
 console.log(date);
+
 
 let year = date.getFullYear();
 console.log(year);
 
+
+//selecting element in html and DOM manipulation
 document.querySelector('h1').textContent = year;
 
 document.querySelector('div').innerHTML = '<h2>Subtitle</h2>';
@@ -13,3 +17,4 @@ newpara.textContent = 'This is a paragraph';
 document.querySelector('div').appendChild(newpara);
 
 document.querySelector('img').setAttribute('src', 'images/jiraya.jpeg')
+document.querySelector('img').classList.add('add_border')
