9401c135824f4a9bcfe2528671eae557b8f26e52
Josh Skouson <Jmatt6836@gmail.com>
2022-05-14 13:47:41 -0600

    added date to top

--- WDD230/scripts/flex.js
+++ WDD230/scripts/flex.js
@@ -3,3 +3,15 @@ let hambutton = document.querySelector('.ham');
 let mainnav = document.querySelector('nav');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')});
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
