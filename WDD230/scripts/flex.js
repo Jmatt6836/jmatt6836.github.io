cc9d105cb467a8b7f8139e1d6db0b76b003ef8ea
Josh Skouson <Jmatt6836@gmail.com>
2022-05-21 17:09:08 -0600

    more changes to commerce page

--- WDD230/scripts/flex.js
+++ WDD230/scripts/flex.js
@@ -15,3 +15,14 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 // long, medium, short options ... try them
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+const week_names = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday'];
+const weekname = week_names[now.getDay()];
+
+
+const meetingAlert = document.querySelector('.meeting-alert');
+if (weekname === 'Monday' || weekname === 'Tuesday'){
+    meetingAlert.style.display = "block";
+} else {
+    meetingAlert.style.display = "none";
+}
---
+++ WDD230/scripts/position.js
@@ -0,0 +1,5 @@
+let weekday = getDay()
+
+if (weekday == 1|| weekday == 2){
+    let output = document
+}
