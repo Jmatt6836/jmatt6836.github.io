7390c48099d4c38d2bb71cfca27167b0cb4dbd5e
Josh Skouson <Jmatt6836@gmail.com>
2022-05-27 12:05:43 -0600

    adding windchill and other stuff

---
+++ WDD230/chamber/scripts/windchill.js
@@ -0,0 +1,13 @@
+let wind = document.getElementById("wind").textContent;
+let temp = document.getElementById("temp").textContent;
+console.log(temp);
+console.log(wind);
+
+if (temp <= 50 && wind >= 3){
+let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(wind,0.16)) + (0.4275*temp*Math.pow(wind,0.16)));
+document.getElementById("chill").textContent = chill.toFixed(2);
+}
+else{
+    let chill = document.getElementById("chill").textContent = "N/A"
+    
+}
