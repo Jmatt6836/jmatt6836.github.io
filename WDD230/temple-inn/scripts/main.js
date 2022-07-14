88c5675b91e36b3ce0f4ee241903fa9239cc0c39
Josh Skouson <Jmatt6836@gmail.com>
2022-07-13 20:01:30 -0600

    making pages, setting up css and js, start design

---
+++ WDD230/temple-inn/scripts/main.js
@@ -0,0 +1,46 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+const apiURL = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=38.9807&lon=-77.1003&appid=6b200362483001f5607d6b21dbdd2ef0'
+
+fetch(apiURL)
+    .then((response) => response.json())
+        .then((jsObject) => {
+        // console.log(jsObject.weather[0].description);
+        console.log(jsObject)
+        document.querySelector('#current-temp').textContent = (jsObject.main.temp * 9/5 - 459.67).toFixed(1);
+        document.querySelector('#wind').textContent = jsObject.wind.speed.toFixed(1);
+        // document.querySelector('#chill').textContent = (jsObject.main.feels_like * 9/5 - 459.67).toFixed(1);
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#weather-icon').setAttribute('src', iconsrc);
+    document.querySelector('#weather-icon').setAttribute('alt', desc);
+    document.querySelector('#fig-caption').textContent = desc;
+  });
+
+
