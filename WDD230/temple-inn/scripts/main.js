7cbef8da7c5496dc3d5314f3001b3df2da2b3e2a
Josh Skouson <Jmatt6836@gmail.com>
2022-07-15 12:02:05 -0600

    Major changes, finished making pages

--- WDD230/temple-inn/scripts/main.js
+++ WDD230/temple-inn/scripts/main.js
@@ -25,16 +25,17 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-const apiURL = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=38.9807&lon=-77.1003&appid=6b200362483001f5607d6b21dbdd2ef0'
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=4348599&appid=4d34a8f41d31a981e9b52f710ec143c8'
 
 fetch(apiURL)
     .then((response) => response.json())
         .then((jsObject) => {
-        // console.log(jsObject.weather[0].description);
+        console.log(jsObject.main)
         console.log(jsObject)
         document.querySelector('#current-temp').textContent = (jsObject.main.temp * 9/5 - 459.67).toFixed(1);
         document.querySelector('#wind').textContent = jsObject.wind.speed.toFixed(1);
-        // document.querySelector('#chill').textContent = (jsObject.main.feels_like * 9/5 - 459.67).toFixed(1);
+        document.querySelector('#chill').textContent = (jsObject.main.feels_like * 9/5 - 459.67).toFixed(1);
+        document.querySelector('#humid').textContent = jsObject.main.humidity;
 
     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
