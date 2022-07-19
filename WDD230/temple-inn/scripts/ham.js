58ceef33c79f0703b5aa7ed5f5beeead7bbb3ad6
Josh Skouson <Jmatt6836@gmail.com>
2022-07-19 15:43:06 -0600

    fixing more stuff

---
+++ WDD230/temple-inn/scripts/ham.js
@@ -0,0 +1,5 @@
+let hambutton = document.querySelector('.ham');
+
+let mainnav = document.querySelector('nav');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')});
--- WDD230/temple-inn/scripts/main.js
+++ WDD230/temple-inn/scripts/main.js
@@ -45,3 +45,4 @@ fetch(apiURL)
   });
 
 
+
